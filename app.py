89f32f03a7ccba9ca7793c484b44959672ebeffe
Muhammad Khattab <msskzx@gmail.com>
2019-06-06 18:16:25 +0200

    bug fix, printing playlists names

--- app.py
+++ app.py
@@ -8,20 +8,19 @@ load_dotenv()
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
-redirect_uri= os.getenv('REDIRECT_URI')
 username= os.getenv('USERNAME')
-
-print(username)
-
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-scope = 'user-library-read playlist-read-private'
+redirect_uri= os.getenv('REDIRECT_URI')
 
 try:
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    scope = 'user-library-read playlist-read-private'
+
     token = util.prompt_for_user_token(username, scope,client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
     spotify = spotipy.Spotify(auth=token)
-    playlists = spotify.user_playlists(username)
 
+    results = spotify.user_playlists(username)
+    playlists = results['items']
     for playlist in playlists:
-        print(playlist)
+        print(playlist['name'])
 except:
     print('Token is not accesible for ' + username)
